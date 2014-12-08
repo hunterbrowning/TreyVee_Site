12c66de9a994a19d6ad2eaa795eb5ecc6b0a0afb
Hunter Browning <hpbrowning93@gmail.com>
2014-12-08 02:09:37 -0600

    up and down color changing

--- assets/js/ko_model.js
+++ assets/js/ko_model.js
@@ -3,6 +3,7 @@ function ViewModel() {
    var hueArray = [198, 203, 220, 234];
    var colorArray = ["hsl(198, 100%, 66%)", "hsl(203, 95%, 54%)", "hsl(220, 91%, 50%)", "hsl(234, 93%, 47%)"]
    var colorCounter = 0;
+   var hueForward = true;
    this.Hue = ko.observable(198);
    // Sat & Light are not currently changing
    self.Sat = ko.observable(100);
@@ -20,16 +21,24 @@ function ViewModel() {
    self.hslrizer = ko.observable("hsl(198, 100%, 66%)");
 
    self.colorStepper = function(){
-      this.hslrizer(colorArray[colorCounter]);
-      if (colorCounter == 3){
-         colorCounter = 0;
-      } else{
-         colorCounter++;
+      if (hueForward){
+         if (colorCounter == 3){
+            hueForward = false;
+         } else{
+            colorCounter++;
+         }
+      } else {
+         if (colorCounter == 0){
+            hueForward = true;
+         } else{
+            colorCounter--;
+         }
       }
+      this.hslrizer(colorArray[colorCounter]);
+      
    };
 
-   var hueForward = true;
-   setInterval(this.colorStepper, 500);
+   setInterval(this.colorStepper, 250);
 };
 
 ko.applyBindings(ViewModel());
